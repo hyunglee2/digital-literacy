e572b1b9baff0c233e305d39ae702a4e8da85262
hyunglee2 <hihylee2@gmail.com>
2023-10-16 03:57:54 +0900

    json format define

--- common/js/localstorage.js
+++ common/js/localstorage.js
@@ -1,21 +1,68 @@
-// JSON 형태
-let text = '{"acti_data":[' +
-    '{"talk1":"","talk2":"","talk3":"" },' +
-    '{"msg1":"","msg2":"","msg3":"" },' +
-    '{"cafe1":"","cafe2":"","cafe3":""},' +
-    '{"blog1":"","blog2":"","blog3":"" },' +
-    '{"video1":"","video2":"","video3":"","video4":"" },' +
-    '{"news1":"","news2":"","news3":"","news4":"" },' +
-    '{"play_time":"" }  ]} ';
+//로컬에 저장할 때는 문자열로 바꾸기  (stringify)
+// ex) const parse_data = JSON.parse(localStorage.getItem(player));//지금 사용자 데이터
+//localStorage.setItem(player, JSON.stringify(parse_data));  //문자열로 바꿔서 로컬저장
+// ex) JSON 수정할 때는 객체로 바꾸기      (parse)
 
-// let result = '{"acti_data":[' +
-// '{"talk1":"","talk2":"","talk3":"" },' +
-// '{"msg1":"","msg2":"","msg3":"" },' +
-// '{"cafe1":"","cafe2":"","cafe3":""},' +
-// '{"blog1":"","blog2":"","blog3":"" },' +
-// '{"video1":"","video2":"","video3":"","video4":"" },' +
-// '{"news1":"","news2":"","news3":"","news4":"" },' +
-// '{"play_time":"" }  ]} ';
+
+// JSON 형태
+const user_data = `{
+    "user_data": [
+      {
+        "71succ": "",
+        "71fail": "",
+        "71ded": ""
+      },
+      {
+        "72succ": "",
+        "72fail": "",
+        "72ded": ""
+      },
+      {
+        "73succ": "",
+        "73fail": "",
+        "73ded": ""
+      },
+      {
+        "74succ": "",
+        "74fail": "",
+        "74ded": ""
+      },
+      {
+        "75succ": "",
+        "75fail": "",
+        "75ded": ""
+      },
+      {
+        "sum": ""
+      }
+    ]
+  }`;
+
+// 현재 페이지의 URL을 가져와 경로부분 추출, 확장자는 제거
+var currURL = window.location.href;
+var pathArray = currURL.split('/');
+var currPage = pathArray[pathArray.length - 1].replace(".html", "");
+console.log("현재 HTML 파일 이름: " + currPage);
+var currScn;
+
+// switch 문 대신 if-else if 구문 사용
+if (currPage === "map") {
+    currScn = 71;
+    parsingNum = 0;
+} else if (currPage === "train") {
+    currScn = 72;
+    parsingNum = 1;
+} else if (currPage === "order") {
+    currScn = 73;
+    parsingNum = 2;
+} else if (currPage === "talk") {
+    currScn = 74;
+    parsingNum = 3;
+} else if (currPage === "bank") {
+    currScn = 75;
+    parsingNum = 4;
+} 
+
 
 
 // index.html 유저 아이디 입력
@@ -24,28 +71,13 @@ const userId = document.getElementById("userId");
 
 idSubmitBtn.addEventListener("click", function () {
     const inputName = userId.value;
-    alert('click');
-
     if (inputName == "") {
-        alert("이름을 입력해 주세요!")
+        $('#staticBackdrop').modal('show');
+        $('.modal-body').html('사용자 ID를 입력해야 버튼이 활성화됩니다.');
         return false;
     } else {
-
-        localStorage["player_now"] = inputName;
-        localStorage[inputName] = text;
-        //const player = localStorage.getItem("player_now");
-        //const test_data = localStorage.getItem('player');
-        timeInput();
-        location.href = "TypeSelection.html";
+        localStorage["user_now"] = inputName;
+        localStorage[inputName] = user_data;
+        location.href = "./main/main.html";
     }
 })
-
-
-function timeInput() {
-    //타임스탬프 저장
-    const player = localStorage.getItem('player_now');//지금 사용자 누구
-    const parse_data = JSON.parse(localStorage.getItem(player));//지금 사용자 데이터
-    const timeData = parse_data.acti_data[6].play_time;
-    parse_data.acti_data[6].play_time = returnDate;
-    localStorage.setItem(player, JSON.stringify(parse_data));  //문자열로 바꿔서 로컬저장
-}
